beb4b37f7aca2196b20590cea4f5081c85046a2b
chrystal <cltacker13@gmail.com>
2023-08-15 20:39:50 -0400

    initial commit with firebase addons

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
+import { initializeApp } from 'firebase/app';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider, SafeAreaView, initialWindowMetrics } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Pressable, Alert, Button } from 'react-native';
 import React from 'react';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdjPP6oTh9WMWgD6fNqvUr0YBgns3dIZ8",
+  authDomain: "theconnect4app.firebaseapp.com",
+  databaseURL: "https://theconnect4app-default-rtdb.firebaseio.com",
+  projectId: "theconnect4app",
+  storageBucket: "theconnect4app.appspot.com",
+  messagingSenderId: "958341876928",
+  appId: "1:958341876928:web:5ca6d83834f6fe32e37125"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 const Stack = createNativeStackNavigator();
 const logoImage = require ('./assets/images/connect4Logo.png');
