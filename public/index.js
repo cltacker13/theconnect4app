484aa461e2968816e32cb3ec74d914d109fd75c5
chrystal <cltacker13@gmail.com>
2023-08-15 22:01:51 -0400

    trying to confirm app js included

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
-import { initializeApp } from 'firebase/app';
+import { App } from '/App.js';
+//import { initializeApp } from 'firebase/app';
 import { createRoot } from 'react-dom/client'
 import React from 'react';
-import { App } from '/App.js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 
 const root = createRoot(document.getElementById("root"));
 root.render(App);
