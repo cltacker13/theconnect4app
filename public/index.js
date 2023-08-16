3050ba22bfb70d84ae2692874ecd5a84adf0e5d6
chrystal <cltacker13@gmail.com>
2023-08-15 23:19:50 -0400

    removed dup appjs file and added placeholder code

--- public/index.js
+++ public/index.js
@@ -1,5 +1,13 @@
-//import { App } from '../App.js';
-import { App } from './App.js';
+//import { App } from '../App.js'; //can't find the file outside of public
+
+//if app.js is in public it finds it, but does not render
+//import { App } from './App.js'; 
 
 const root = document.getElementById('root');
 root.innerHTML = App();
+
+function App() {
+    return (
+        '<h1>theConnect4App</h1>'
+    );
+}
