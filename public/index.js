437673db31607fe25625f07169f1b7f2d588e1c4
chrystal <cltacker13@gmail.com>
2023-08-15 22:39:16 -0400

    edit import location

--- public/index.js
+++ public/index.js
@@ -1,20 +1,5 @@
-//import { initializeApp } from 'firebase/app';
-//import { createRoot, hydrateRoot  } from 'react-dom/client';
-import { App } from './App.js';
-/*
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCdjPP6oTh9WMWgD6fNqvUr0YBgns3dIZ8",
-  authDomain: "theconnect4app.firebaseapp.com",
-  databaseURL: "https://theconnect4app-default-rtdb.firebaseio.com",
-  projectId: "theconnect4app",
-  storageBucket: "theconnect4app.appspot.com",
-  messagingSenderId: "958341876928",
-  appId: "1:958341876928:web:5ca6d83834f6fe32e37125"
-};
-*/
-// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
+import { App } from '..App.js';
+
 
 const root = document.getElementById('root');
 root.innerHTML = App;
